093379f09560b08dd1d59acfbb319305e8d15c7b
Beshr Al Nahas <beshr.ns@gmail.com>
2015-05-08 16:00:52 +0200

    The updated install script

---
+++ raspi/scripts/cc2538dk/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+tty_path=`ls /dev/serial/by-id/usb-FTDI_FT231X_USB_UART_*`
+firmware_path=$1
+# Now reset and program the node
+~/scripts/cc2538dk/setupCC2538.py
+sleep 1
+~/scripts/cc2538dk/cc2538-bsl/cc2538-bsl.py -b 250000 -p $tty_path -e -v -w $firmware_path
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+sleep 1
+# Reboot the node
+usb-hub-off.sh
+usb-hub-on.sh
+sleep 1
