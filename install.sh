9b6030fa11a85f56ca05ae4d971d26699d484ff1
Beshr Al Nahas <beshr@chalmers.se>
2018-08-08 23:16:16 +0200

    Fix paths

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 rsync -azv --delete server/scripts /usr/testbed/
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
+parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
