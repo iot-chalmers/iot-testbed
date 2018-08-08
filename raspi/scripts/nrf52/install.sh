e0535c31d202e9fba7ebed46a5a048e8b188ad3e
Beshr Al Nahas <beshr@chalmers.se>
2018-08-08 19:57:24 +0200

    Adding JLink tools for NRF on ARM-Linux

--- raspi/scripts/nrf52/install.sh
+++ raspi/scripts/nrf52/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 # First reset the node so we get it mounted if it was not
 #sleep 1
 usb-hub-off.sh
@@ -7,12 +7,13 @@ usb-hub-on.sh
 sleep 1
 # Now program the node
 firmware_path=$1
-nrfjprog.sh -f nrf52 --erase-all
-nrfjprog.sh --flash $firmware_path -f nrf52
+#get script from https://raw.githubusercontent.com/WayneKeenan/nrfjprog.sh/master/nrfjprog.sh
+./JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
+./JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
 if [ $? -ne 0 ]; then
     exit 1
 fi
-nrfjprog.sh --reset -f nrf52
+./JLink_Linux_V632i_arm/nrfjprog.sh --reset -f nrf52
 sleep 1
 # Reboot the node
 usb-hub-off.sh
