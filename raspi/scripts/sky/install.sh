93b126a6ccfc53ff94f6207d0d94c095ae706272
Beshr Al Nahas <beshr@chalmers.se>
2018-08-08 22:47:42 +0200

    Fix paths

--- raspi/scripts/sky/install.sh
+++ raspi/scripts/sky/install.sh
@@ -2,22 +2,20 @@
 #search for the Korean clone CM5000MSP and the original Moteiv TMote
 tty_path1=`ls /dev/serial/by-id/usb-FTDI_MTM-CM5000MSP_*`
 tty_path2=`ls /dev/serial/by-id/usb-Moteiv_tmote_sky_*`
-tty_path=$(tty_path1)$(tty_path2)
+tty_path="$tty_path1$tty_path2"
 firmware_path=$1
-BSL="/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb"
-BSL_FILETYPE=-I
-IHEXFILE=$firmware_path
+#BSL="/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb"
 #reset
-$(BSL) -c $tty_path -r
+/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb -c $tty_path -r
 #erase
-$(BSL) -c $tty_path -e && sleep 2 ; 
+/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb -c $tty_path -e && sleep 2 ; 
 #program
-$(BSL) -c $tty_path $(BSL_FILETYPE) -p $(IHEXFILE) && sleep 2 
+/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb -c $tty_path -I -p $firmware_path && sleep 2 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 #reset
-$(BSL) -c $tty_path -r
+/home/user/scripts/sky/skytools/msp430-bsl-linux --telosb -c $tty_path -r
 # Reboot the node
 #usb-hub-off.sh
 #usb-hub-on.sh
