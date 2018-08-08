2410799b6717d75a6ce8ac56a7768b71b2b84014
Beshr Al Nahas <beshr@chalmers.se>
2018-08-08 21:46:33 +0200

    Fix path to the command USB on/off - try 3

--- raspi/scripts/usb-hub-off.sh
+++ raspi/scripts/usb-hub-off.sh
@@ -1,5 +1,5 @@
-sudo hub-ctrl -h 0 -P 2 -p 0
-sudo hub-ctrl -h 0 -P 3 -p 0
-sudo hub-ctrl -h 0 -P 4 -p 0
-sudo hub-ctrl -h 0 -P 5 -p 0
+sudo /home/user/scripts/hub-ctrl -h 0 -P 2 -p 0
+sudo /home/user/scripts/hub-ctrl -h 0 -P 3 -p 0
+sudo /home/user/scripts/hub-ctrl -h 0 -P 4 -p 0
+sudo /home/user/scripts/hub-ctrl -h 0 -P 5 -p 0
 
