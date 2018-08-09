8625b9a82a5beace43326de8ad81d829d052ecef
Beshr Al Nahas <beshr@chalmers.se>
2018-08-09 16:47:12 +0200

    Use screen with picocom as nohup does not work with it

--- raspi/scripts/nrf52/install.sh
+++ raspi/scripts/nrf52/install.sh
@@ -8,7 +8,6 @@ sleep 1
 # Now program the node
 firmware_path=$1
 #get script from https://raw.githubusercontent.com/WayneKeenan/nrfjprog.sh/master/nrfjprog.sh
-#pushd /home/user/scripts/nrf52/JLink_Linux_V632i_arm
 export PATH=$PATH:/home/user/scripts/nrf52/JLink_Linux_V632i_arm
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
@@ -16,8 +15,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --reset -f nrf52
-sleep 1
+sleep 0.5
 # Reboot the node
-/home/user/scripts/usb-hub-off.sh
-/home/user/scripts/usb-hub-on.sh
-sleep 1
+#/home/user/scripts/usb-hub-off.sh
+#/home/user/scripts/usb-hub-on.sh
+#sleep 1
--- server/scripts/nrf52/stop.py
+++ server/scripts/nrf52/stop.py
@@ -34,7 +34,9 @@ if __name__=="__main__":
        
   hosts_path = os.path.join(job_dir, "hosts")
   # Kill serialdump
-  pssh(hosts_path, "killall picocom -9", "Stopping picocom")
+  #pssh(hosts_path, "killall picocom -9", "Stopping picocom")
+  pssh(hosts_path, "screen -S nrf52screen -X quit", "Stopping screen")
+
   # Program the nodes with null firmware
   if pssh(hosts_path, "%s %s"%(os.path.join(REMOTE_JN_SCRIPTS_PATH, "install.sh"), REMOTE_NULL_FIRMWARE_PATH), "Uninstalling nrf52 firmware") != 0:
     sys.exit(4)
