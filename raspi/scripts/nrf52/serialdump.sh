5960dadeb4b69c1487fc1c0b07da21793d8f05f7
Beshr Al Nahas <beshr@chalmers.se>
2018-08-09 16:47:12 +0200

    Use screen with picocom as nohup does not work with it

--- raspi/scripts/nrf52/serialdump.sh
+++ raspi/scripts/nrf52/serialdump.sh
@@ -2,9 +2,9 @@
 
 log_path=$1
 tty_path=`ls /dev/serial/by-id/usb-SEGGER_J-Link_*`
-#gtimeout -s ABRT --kill-after=$(($(DURATION)+2)) --foreground $(DURATION) picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path --logfile $log_path
-#nohup ~/scripts/contiki-serialdump -b115200 $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
-nohup picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path --logfile $log_path & > /dev/null 2> /dev/null
+screen -wipe
+screen -dmS nrf52screen bash
+screen -S nrf52screen -X stuff "picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path > $log_path\n"
 
 sleep 1
 ps | grep "$! "
