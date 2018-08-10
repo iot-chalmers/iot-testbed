62dc5c580f694c42ebabbfbb4f388af8b0e54afb
Beshr Al Nahas <beshr@chalmers.se>
2018-08-10 21:46:16 +0200

    streamline logging

--- raspi/scripts/nrf52/serialdump.sh
+++ raspi/scripts/nrf52/serialdump.sh
@@ -4,7 +4,8 @@ log_path=$1
 tty_path=`ls /dev/serial/by-id/usb-SEGGER_J-Link_*`
 screen -wipe
 screen -dmS nrf52screen bash
-screen -S nrf52screen -X stuff "picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path > $log_path\n"
+#screen -S nrf52screen -X stuff "picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
+screen -S nrf52screen -X stuff "~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
 
 sleep 1
 ps | grep "$! "
