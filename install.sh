176d61f2a28c4aa0e04b815fec8bf8cb220f31ca
Beshr Al Nahas <beshr@chalmers.se>
2018-08-10 21:49:46 +0200

    git pull in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+git pull
 rsync -azv --delete server/scripts /usr/testbed/
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
