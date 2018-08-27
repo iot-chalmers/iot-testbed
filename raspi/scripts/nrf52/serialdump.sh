a47d291856d727ae63e80fb47d2999491b254a98
Beshr Al Nahas <beshr@chalmers.se>
2018-08-27 19:47:30 +0200

    using native linux cat instead of contiki-serialdump due to corrupted lines

--- raspi/scripts/nrf52/serialdump.sh
+++ raspi/scripts/nrf52/serialdump.sh
@@ -6,8 +6,9 @@ tty_path=`ls /dev/serial/by-id/usb-SEGGER_J-Link_*`
 #screen -dmS nrf52screen bash
 #screen -S nrf52screen -X stuff "picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
 #screen -S nrf52screen -X stuff "~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
-nohup ~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
-
+#nohup ~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
+stty 230400 crtscts cs8
+nohup sh -c "cat $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null" 
 sleep 1
 ps | grep "$! "
 exit $?
