4e7f6ced45061046c15c1d8facf833efd7752ee2
Beshr Al Nahas <beshr@chalmers.se>
2018-09-06 17:13:34 +0200

    UART baudrate: 460800

--- raspi/scripts/nrf52/serialdump.sh
+++ raspi/scripts/nrf52/serialdump.sh
@@ -6,7 +6,7 @@ killall -9 picocom
 killall -9 screen
 screen -wipe
 screen -dmS nrf52screen bash
-screen -S nrf52screen -X stuff "picocom --noreset -fh -b 230400 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
+screen -S nrf52screen -X stuff "picocom --noreset -fh -b 460800 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
 #screen -S nrf52screen -X stuff "~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
 #nohup ~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
 #stty 230400 crtscts cs8
