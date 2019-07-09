f977f09f730bda136f2d5e97ca407648a021b9f5
Beshr Al Nahas <beshr@chalmers.se>
2019-07-09 19:23:49 +0200

    copy examples in the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 git pull
+rsync -azv --delete server/examples /usr/testbed/
 rsync -azv --delete server/scripts /usr/testbed/
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
