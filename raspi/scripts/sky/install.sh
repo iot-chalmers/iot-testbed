d9df5f50320c45511edf84a68c5dd80a65517425
Beshr Al Nahas <beshr@chalmers.se>
2019-07-11 11:14:01 +0200

    Fix: Sky platform serial name

--- raspi/scripts/sky/install.sh
+++ raspi/scripts/sky/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #search for the Korean clone CM5000MSP and the original Moteiv TMote
-tty_path1=`ls /dev/serial/by-id/usb-FTDI_MTM-CM5000MSP_*`
+tty_path1=`ls /dev/serial/by-id/usb-FTDI_MTM-CM5000*`
 tty_path2=`ls /dev/serial/by-id/usb-Moteiv_tmote_sky_*`
 tty_path="$tty_path1$tty_path2"
 firmware_path=$1
