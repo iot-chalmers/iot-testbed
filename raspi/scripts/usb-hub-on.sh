312f40b998c00849c283971f9a69adf22413b259
Beshr Al Nahas <beshr@chalmers.se>
2019-07-11 15:50:30 +0200

    fix usb-ctrl script

--- raspi/scripts/usb-hub-on.sh
+++ raspi/scripts/usb-hub-on.sh
@@ -1,5 +1,5 @@
 sudo /home/user/scripts/hub-ctrl -h 0 -P 2 -p 1
 sudo /home/user/scripts/hub-ctrl -h 0 -P 3 -p 1
-sudo /home/user/scripts/hub-ctrl -h 0 -P 4 -p 1
-sudo /home/user/scripts/hub-ctrl -h 0 -P 5 -p 1
+#sudo /home/user/scripts/hub-ctrl -h 0 -P 4 -p 1
+#sudo /home/user/scripts/hub-ctrl -h 0 -P 5 -p 1
 
