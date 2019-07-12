f6fc4568b672ee5ed7dc5e66801a8b94062825d4
Beshr Al Nahas <beshr@chalmers.se>
2019-07-12 12:05:16 +0200

    update install instructions

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -2,6 +2,7 @@
 ########## To sync: rsync -avz -e "ssh -p17122" ./iot-testbed testbed@sunlight.ds.informatik.uni-kiel.de:/home/testbed ######
 
 ########## Server side ###########
+##### Make a user called testbed that have sudo, and execute thses steps from user tesbed
 ##### 1. configure the /etc/hosts file to have the raspi IPs and hostnames, for example:
 # 192.168.87.228	raspi03
 # 192.168.87.229	raspi04
@@ -24,7 +25,7 @@ sudo pip install pytz
 sudo wget https://linuxnet.ca/ieee/oui.txt -O /usr/local/etc/oui.txt
 
 ####ping PIs
-for ip in $(cat ./server/scripts/all-hosts); do ping -c 1 -t 1 $ip && echo "${ip} is up"; done
+for ip in $(cat ./server/scripts/all-hosts); do ping -c 1 -t 1 $ip && echo "${ip} is up"; done | grep "is up"
 
 ####add user 'testbed' to dialout group
 sudo usermod -aG dialout testbed
@@ -44,7 +45,7 @@ sudo chown testbed:testbed /usr/testbed
 ##### b. please make a user called 'user', and give it sudo access without password over ssh (add it to sudoers)
 ####### add the following line in /etc/sudoers:
 ####### user    ALL=(ALL) NOPASSWD: ALL
-##### (done automatically in step 7) c. ssh once to every PI, such that you have the key signature in your .ssh folder, and it does not complain later
+##### (done automatically in step 2) c. ssh once to every PI, such that you have the key signature in your .ssh folder, and it does not complain later
 ##### d. if you get the following strange error message on the PIa: 
 ######(mesg: ttyname failed: Inappropriate ioctl for device), delete the last line in /root/.profile: (mesg n || true)
 ######The file shall look like this (roughly), without the offending line: mesg n || true:
@@ -56,37 +57,35 @@ sudo chown testbed:testbed /usr/testbed
 #   fi
 # fi
 
-####2. these commands would install the required pkgs on the PIs
-parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo mkdir -p /usr/testbed && sudo chown user:user /usr/testbed && sudo usermod -aG dialout user && sudo apt update && sudo apt -y --force-yes install picocom ssh python2.7 python-serial screen at ntpdate ntp"
-
-####3. install testbed SW to the server /usr and to PIs
-sh /home/testbed/iot-testbed/install.sh
-
-####4. COPY JLINK rules file to allow flashing nrf52 for non-root users
-parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo cp /home/user/scripts/nrf52/JLink_Linux_V632i_arm/99-jlink.rules /etc/udev/rules.d"
-
-####5. IMPROTANT: login as root or sudo -i to each PI, and flash any file on the nrf52 board to update the JLink firmwarw
-parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo -i && cd /home/user/scripts/nrf52 && ./install.sh null.nrf52.hex"
-
-####6. add PIs keys signatures -- execute on every user account on the server
+####2. add PIs keys signatures -- execute on every user account on the server
 for ip in $(cat /usr/testbed/scripts/all-hosts); do 
   if [ -z `ssh-keygen -F $ip` ]; then
     ssh-keyscan -H $ip >> ~/.ssh/known_hosts
   fi
 done
 
+####3. these commands would install the required pkgs on the PIs
+parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo mkdir -p /usr/testbed && sudo chown user:user /usr/testbed && sudo usermod -aG dialout user && sudo apt update && sudo apt -y --force-yes install picocom ssh python2.7 python-serial screen at ntpdate ntp"
+
+####4. install testbed SW to the server /usr and to PIs
+sh /home/testbed/iot-testbed/install.sh
+
+####5. COPY JLINK rules file to allow flashing nrf52 for non-root users
+parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo cp /home/user/scripts/nrf52/JLink_Linux_V632i_arm/99-jlink.rules /etc/udev/rules.d"
+
+####6. IMPROTANT: login as root or sudo -i to each PI, and flash any file on the nrf52 board to update the JLink firmwarw
+parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo -i && cd /home/user/scripts/nrf52 && ./install.sh null.nrf52.hex"
+
 ####7. test if testbed SW works and connects to the PIs listed under 
 python /usr/testbed/scripts/testbed.py status
 python /usr/testbed/scripts/testbed.py create --name 'null' --platform 'nrf52' --duration 2 --copy-from /usr/testbed/examples/nrf52-hello-world/hello-world.nrf52.hex --start
 
-
-
 ####8. Optional, but recommended: check ntp time. SW will break when the PIs time is out of sync.
-parallel-ssh --hosts ./server/scripts/all-hosts --user user  --inline "ntptime"
+# parallel-ssh --hosts ./server/scripts/all-hosts --user user  --inline "ntptime"
 
 ####9. test making a job from your laptop using your own account on the server: (replace the variables with usseful arguments)
-# scp -P17122 $(FNAME).hex ban@sunlight.ds.informatik.uni-kiel.de:/home/ban/newjob.nrf52.hex
-# ssh -p17122 ban@sunlight.ds.informatik.uni-kiel.de "python /usr/testbed/scripts/testbed.py create --name '${NAME}' --platform 'nrf52' --duration ${DURATION} --copy-from /home/ban/newjob.nrf52.hex --start"
+# scp -P17122 $(FNAME).hex ${USER}@sunlight.ds.informatik.uni-kiel.de:/home/${USER}/newjob.nrf52.hex
+# ssh -p17122 ${USER}@sunlight.ds.informatik.uni-kiel.de "python /usr/testbed/scripts/testbed.py create --name '${NAME}' --platform 'nrf52' --duration ${DURATION} --copy-from /home/${USER}/newjob.nrf52.hex --start"
 
 ####10. if this makes a problem, then disable apt autoupdate. Steps:
 # systemctl stop apt-daily.service
@@ -97,3 +96,14 @@ parallel-ssh --hosts ./server/scripts/all-hosts --user user  --inline "ntptime"
 # do
 #   sleep 1;
 # done
+
+##### Appendix: Adding a new user to the server
+####Note that tsetbed SW is installed under /usr/testbed, and the sources (git hub repo) is under /home/testbed/iot-testbed
+####1. add the users that need to use the testbedSW to the group testbed
+sudo usermod -aG testbed newuser
+####2. add PIs keys signatures -- execute on every user account on the server
+for ip in $(cat /usr/testbed/scripts/all-hosts); do 
+  if [ -z `ssh-keygen -F $ip` ]; then
+    ssh-keyscan -H $ip >> ~/.ssh/known_hosts
+  fi
+done
