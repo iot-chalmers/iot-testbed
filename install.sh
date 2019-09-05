ec5711d23a2ddfef78ba1aefdefc7a24ed6aa66e
Valentin Poirot <poirotv@chalmers.se>
2019-09-05 16:41:59 +0200

    first modif to improve sky support for the testbed

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ git pull
 rsync -azv --delete server/examples /usr/testbed/
 rsync -azv --delete server/scripts /usr/testbed/
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
+parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/fstab /etc/fstab
+parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo mount -a"
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
