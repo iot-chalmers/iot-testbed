54f68a4fff1dd074cc711d9ff36280e4f15bdd5f
Valentin Poirot <poirotv@chalmers.se>
2019-09-06 10:33:41 +0200

    download can crash, causing stop to fail?

--- server/scripts/sky/download.py
+++ server/scripts/sky/download.py
@@ -53,44 +53,45 @@ if __name__=="__main__":
   if len(sys.argv)<2:
     print "Job directory parameter not found!"
     sys.exit(1)
-    
-  # The only parameter contains the job directory
-  job_dir = sys.argv[1]
-         
-  # a dict containing all log lines keyed with timestamps
-  all_lines = dict()
   
-  # the file containing the aggregated logs
-  dest_file_path = os.path.join(job_dir, "logs", "log.txt")
-  dest_file = open(dest_file_path, 'w')
-  
-  # the directory containing the logs from all nodes
-  logs_dir = os.path.join(job_dir, "logs")
-  # first open every individual log file
-  log_files = []
-  print "Aggregating logs...",
-  sys.stdout.flush()
-  for f in os.listdir(logs_dir):
-    if os.path.isdir(os.path.join(logs_dir, f)):
-      # extract node_id from file name (e.g. "pi16" -> 16)
-      node_id = extract_pi_number(f)
+  try:
+    # The only parameter contains the job directory
+    job_dir = sys.argv[1]
 
-      log_file_path = os.path.join(logs_dir, f, "log.txt")
-      curr_log_file = open(log_file_path, 'r')
+    # a dict containing all log lines keyed with timestamps
+    all_lines = dict()
+
+    # the file containing the aggregated logs
+    dest_file_path = os.path.join(job_dir, "logs", "log.txt")
+    dest_file = open(dest_file_path, 'w')
+
+    # the directory containing the logs from all nodes
+    logs_dir = os.path.join(job_dir, "logs")
+    # first open every individual log file
+    log_files = []
+    print "Aggregating logs...",
+    sys.stdout.flush()
+    for f in os.listdir(logs_dir):
+      if os.path.isdir(os.path.join(logs_dir, f)):
+        # extract node_id from file name (e.g. "pi16" -> 16)
+        node_id = extract_pi_number(f)
+
+        log_file_path = os.path.join(logs_dir, f, "log.txt")
+        curr_log_file = open(log_file_path, 'r')
 	  
-      timestamp, message = parse_next_line(curr_log_file)
-      log_files.append({'node_id': node_id, 'file': curr_log_file, 'next_timestamp': timestamp, 'next_message': message})
-  
-  line_cnt = 0
-  while True:
-    timestamp, node_id, message = get_next_line(log_files)
-    if timestamp == None:
-      break
-    # generate a single aggregated log file  
-    dest_file.write("%016u\tID:%u\t%s"%(timestamp, node_id, message))
-    line_cnt += 1
-    if line_cnt % 100000 == 0:
-      print "%u"%(line_cnt),
-      sys.stdout.flush()
-	  
-  print "done (%u lines)" %(line_cnt)
+        timestamp, message = parse_next_line(curr_log_file)
+        log_files.append({'node_id': node_id, 'file': curr_log_file, 'next_timestamp': timestamp, 'next_message': message})
+
+    line_cnt = 0
+    while True:
+      timestamp, node_id, message = get_next_line(log_files)
+      if timestamp == None:
+        break
+      # generate a single aggregated log file
+      dest_file.write("%016u\tID:%u\t%s"%(timestamp, node_id, message))
+      line_cnt += 1
+      if line_cnt % 100000 == 0:
+        print "%u"%(line_cnt),
+        sys.stdout.flush()
+
+    print "done (%u lines)" %(line_cnt)
