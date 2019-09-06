3bbba7595bbbdd8008680535e1ad38f93897dec2
Valentin Poirot <poirotv@chalmers.se>
2019-09-06 09:50:04 +0200

    improved install.sh script for ramdisk mounting

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ git pull
 rsync -azv --delete server/examples /usr/testbed/
 rsync -azv --delete server/scripts /usr/testbed/
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/scripts /home/user
-parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/fstab /etc/fstab
+parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/fstab /home/user/fstab
+parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo cp /home/user/fstab /etc/fstab && rm /home/user/fstab && mkdir /home/user/tmp"
 parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo mount -a"
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
