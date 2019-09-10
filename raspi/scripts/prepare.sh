a7b4903241228646e06ce9f2c9a84686960af28b
Valentin Poirot <poirotv@chalmers.se>
2019-09-10 09:12:22 +0200

    killing processes with quiet mode

--- raspi/scripts/prepare.sh
+++ raspi/scripts/prepare.sh
@@ -1,5 +1,6 @@
 /home/user/scripts/usb-hub-on.sh
 mkdir -p logs/$1
 cp -r /home/user/scripts/sky /home/user/tmp/sky
-killall -9 -q picocom
-killall -9 -q serialdump
+killall -q -9 picocom
+killall -q -9 serialdump
+killall -q -9 contiki-timestamp
