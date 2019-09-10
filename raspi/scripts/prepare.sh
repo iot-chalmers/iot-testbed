0ee22532b32bc6bfe4d8c0ec19bc49c5a0b2411b
Valentin Poirot <poirotv@chalmers.se>
2019-09-10 09:42:15 +0200

    check before kill

--- raspi/scripts/prepare.sh
+++ raspi/scripts/prepare.sh
@@ -1,6 +1,6 @@
 /home/user/scripts/usb-hub-on.sh
 mkdir -p logs/$1
 cp -r /home/user/scripts/sky /home/user/tmp/sky
-killall -q -9 picocom
-killall -q -9 serialdump
-killall -q -9 contiki-timestamp
+if pgrep picocom; then killall -9 picocom;fi
+if pgrep serialdump; then killall -9 serialdump;fi
+if pgrep contiki-timestamp; then killall -9 contiki-timestamp;fi
--- server/scripts/sky/stop.py
+++ server/scripts/sky/stop.py
@@ -32,9 +32,9 @@ if __name__=="__main__":
   hosts_path = os.path.join(job_dir, "hosts")
   # Kill serialdump
   #pssh(hosts_path, "killall contiki-serialdump -9", "Stopping serialdump")
-  pssh(hosts_path, '"killall -q -9 serialdump"', "Stopping serialdump")
-  pssh(hosts_path, '"killall -q -9 picocom"', "Stopping picocom")
-  pssh(hosts_path, '"killall -q -9 contiki-timestamp"', "Stopping contiki-timestamp")
+  pssh(hosts_path, '"if pgrep picocom; then killall -9 picocom;fi"', "Stopping serialdump")
+  pssh(hosts_path, '"if pgrep serialdump; then killall -9 serialdump;fi"', "Stopping picocom")
+  pssh(hosts_path, '"if pgrep contiki-timestamp; then killall -9 contiki-timestamp;fi"', "Stopping contiki-timestamp")
   # Program the nodes with null firmware
   if pssh(hosts_path, "%s %s"%(os.path.join(REMOTE_JN_SCRIPTS_PATH, "install.sh"), REMOTE_NULL_FIRMWARE_PATH), "Uninstalling sky firmware") != 0:
     sys.exit(4)
