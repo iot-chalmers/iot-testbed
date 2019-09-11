834c67bec9dfb774ee10491507318654e3dcfd09
Valentin Poirot <poirotv@chalmers.se>
2019-09-11 12:41:01 +0200

    serial_forwarder script was blocking

--- raspi/scripts/sky/serial_forwarder.sh
+++ raspi/scripts/sky/serial_forwarder.sh
@@ -1,14 +1,24 @@
 tty=`ls /dev/serial/by-id/usb-FTDI_MTM-CM5000*`
-port=54321
+port=50000
 if [ -z "$tty" ]
 then
         echo "Could not find sky device"
         exit 1
 fi
+
+# avoid serial port being blocked
+if pgrep picocom; then killall -9 picocom;fi
+if pgrep serialdump; then killall -9 serialdump;fi
+if pgrep serial_forwarder; then killall -9 serial_forwarder;fi
+# kill previous screen
+if pgrep screen; then killall -9 screen;fi
 #set baudrate
 stty -F $tty 115200 min 1 cs8 -cstopb -parenb -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
-# start forwarding
-nc -lt 0.0.0.0 $port <$tty >$tty
-# distant closing will stop netcat
-# reset serial config
-stty -F $tty 9600 min 0 -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
+
+screen -wipe
+screen -dmS skyscreen bash
+screen -S skyscreen -X stuff "nc -lt 0.0.0.0 $port <$tty >$tty\n"
+sleep 1
+
+ps | grep "$! "
+exit $?
