f5c9950c8477102470cd533b58d021dd43589426
Valentin Poirot <poirotv@chalmers.se>
2019-09-11 12:13:08 +0200

    prepare now kills serial_forwarder too

--- raspi/scripts/prepare.sh
+++ raspi/scripts/prepare.sh
@@ -1,6 +1,7 @@
 /home/user/scripts/usb-hub-on.sh
 mkdir -p logs/$1
-cp -r /home/user/scripts/sky /home/user/tmp/sky
+cp -r /home/user/scripts/sky /home/user/tmp
 if pgrep picocom; then killall -9 picocom;fi
+if pgrep serial_forwarder; then killall -9 serial_forwarder;fi
 if pgrep serialdump; then killall -9 serialdump;fi
 if pgrep contiki-timestamp; then killall -9 contiki-timestamp;fi
