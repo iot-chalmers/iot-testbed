2ddd6426cf0ce5a4cbddef66b8dddf0d31e81f1c
Valentin Poirot <poirotv@chalmers.se>
2019-09-19 12:51:41 +0200

    better screen cleaning after serial

--- server/scripts/sky/stop.py
+++ server/scripts/sky/stop.py
@@ -28,6 +28,7 @@ if __name__=="__main__":
   hosts_path = os.path.join(job_dir, "hosts")
   # Kill serialdump
   #pssh(hosts_path, "killall contiki-serialdump -9", "Stopping serialdump")
+  pssh(hosts_path, 'if pgrep screen; then screen -X -S skyscreen quit;fi', "Quitting screen")
   pssh(hosts_path, 'if pgrep picocom; then killall -9 picocom;fi', "Stopping picocom")
   pssh(hosts_path, 'if pgrep serialdump; then killall -9 serialdump;fi', "Stopping serialdump")
   pssh(hosts_path, 'if pgrep serial_forwarder; then killall -9 serial_forwarder;fi; if pgrep cat; then killall -9 cat;fi; if pgrep netcat; then killall -9 netcat;fi; if pgrep tee; then killall -9 tee;fi;', "Stopping serial_forwarder")
