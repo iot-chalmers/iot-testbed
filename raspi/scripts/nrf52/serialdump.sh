f25c2671ecc8f515ee1277081fb66472da5bef4e
Beshr Al Nahas <beshr@chalmers.se>
2019-11-21 16:06:34 +0100

    nrf52 serial: reset the port before opening it

--- raspi/scripts/nrf52/serialdump.sh
+++ raspi/scripts/nrf52/serialdump.sh
@@ -6,11 +6,7 @@ killall -9 picocom
 killall -9 screen
 screen -wipe
 screen -dmS nrf52screen bash
-screen -S nrf52screen -X stuff "picocom --noreset -fh -b 115200 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
-#screen -S nrf52screen -X stuff "~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
-#nohup ~/scripts/contiki-serialdump -b230400 $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null
-#stty 230400 crtscts cs8
-#nohup sh -c "cat $tty_path | ~/scripts/contiki-timestamp > $log_path & > /dev/null 2> /dev/null" 
+screen -S nrf52screen -X stuff "picocom -fh -b 115200 --imap lfcrlf $tty_path | ~/scripts/contiki-timestamp > $log_path\n"
 sleep 1
 ps | grep "$! "
 exit $?
