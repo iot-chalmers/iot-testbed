5a21695e126e7ec71ee2d564cddcc62952d62542
Beshr Al Nahas <beshr@chalmers.se>
2019-11-22 15:52:23 +0100

    nrf52: lower flashing speed to make it more reliable

--- raspi/scripts/nrf52/install.sh
+++ raspi/scripts/nrf52/install.sh
@@ -4,7 +4,7 @@
 #sleep 1
 /home/user/scripts/usb-hub-off.sh
 /home/user/scripts/usb-hub-on.sh
-sleep 0.5
+sleep 1
 # Now program the node
 firmware_path=$1
 #get script from https://raw.githubusercontent.com/WayneKeenan/nrfjprog.sh/master/nrfjprog.sh
@@ -12,11 +12,16 @@ export PATH=$PATH:/home/user/scripts/nrf52/JLink_Linux_V632i_arm
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
 if [ $? -ne 0 ]; then
-    exit 1
+    /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
+    /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
+    if [ $? -ne 0 ]; then exit 1; fi;
 fi
 
 # Reboot the node
 /home/user/scripts/usb-hub-off.sh
 /home/user/scripts/usb-hub-on.sh
-sleep 0.5
+sleep 1
 /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --reset -f nrf52
+sleep 1
+
+# tty=/dev/serial/by-id/usb-SEGGER_J-Link_000683332328-if00; stty -F $tty 115200 min 1 cs8 -cstopb -parenb -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke; cat $tty
