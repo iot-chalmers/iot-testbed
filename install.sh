3eebe0ee48ea542c8bf755b9bf443df1783f7638
Valentin Poirot <poirotv@chalmers.se>
2019-11-25 10:39:10 +0100

    patching baudrate for sky/raspi03 at install

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv raspi/fstab /home/
 parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "if ! grep -q '/home/user/tmp' /etc/fstab; then sudo sh -c 'cat /home/user/fstab >> /etc/fstab'; fi; rm -f /home/user/fstab; mkdir -p /home/user/tmp"
 parallel-ssh --timeout 0 --hosts ./server/scripts/all-hosts --user user --inline "sudo mount -a"
 parallel-rsync -h /usr/testbed/scripts/all-hosts -l user -azv server/scripts /usr/testbed/
+# Patch for Sky on Raspi 03: we need to decrease the baudrate, otherwise install.sh does not install the image but report success
+parallel-ssh --timeout 0 --host raspi03 --user user --inline "sed -i 's/38400/19200/g' /home/user/scripts/sky/install.sh"
