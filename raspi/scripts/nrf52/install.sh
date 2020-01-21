fccf4b41edd4c52407fe4c7c371386ef8fc240d9
Beshr Al Nahas <beshr@chalmers.se>
2020-01-21 12:53:58 +0100

    update nrf jlink to newest verison: 6.60f

--- raspi/scripts/nrf52/install.sh
+++ raspi/scripts/nrf52/install.sh
@@ -5,25 +5,26 @@
 /home/user/scripts/usb-hub-off.sh
 /home/user/scripts/usb-hub-on.sh
 sleep 1
+/home/user/scripts/nrf52/nrfjprog.sh --reset -f nrf52
 # Now program the node
 firmware_path=$1
 #get script from https://raw.githubusercontent.com/WayneKeenan/nrfjprog.sh/master/nrfjprog.sh
-export PATH=$PATH:/home/user/scripts/nrf52/JLink_Linux_V632i_arm
-/home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
-/home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
-/home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
+export PATH=$PATH:/home/user/scripts/nrf52/JLink_Linux_arm
+#/home/user/scripts/nrf52/nrfjprog.sh -f nrf52 --erase-all
+/home/user/scripts/nrf52/nrfjprog.sh --flash $firmware_path -f nrf52
+/home/user/scripts/nrf52/nrfjprog.sh --flash $firmware_path -f nrf52
 if [ $? -ne 0 ]; then
-    /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh -f nrf52 --erase-all
-    /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
-    /home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --flash $firmware_path -f nrf52
+    /home/user/scripts/nrf52/nrfjprog.sh -f nrf52 --erase-all
+    /home/user/scripts/nrf52/nrfjprog.sh --flash $firmware_path -f nrf52
+    /home/user/scripts/nrf52/nrfjprog.sh --flash $firmware_path -f nrf52
     if [ $? -ne 0 ]; then exit 1; fi;
 fi
 
 # Reboot the node
-/home/user/scripts/usb-hub-off.sh
-/home/user/scripts/usb-hub-on.sh
-sleep 1
-/home/user/scripts/nrf52/JLink_Linux_V632i_arm/nrfjprog.sh --reset -f nrf52
-sleep 1
+#/home/user/scripts/usb-hub-off.sh
+#/home/user/scripts/usb-hub-on.sh
+#sleep 1
+/home/user/scripts/nrf52/nrfjprog.sh --reset -f nrf52
+#sleep 1
 
 # tty=/dev/serial/by-id/usb-SEGGER_J-Link_000683332328-if00; stty -F $tty 115200 min 1 cs8 -cstopb -parenb -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke; cat $tty
