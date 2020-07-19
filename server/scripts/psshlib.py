7cff2cf18f071a033262f08b25f73a430a6824b9
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2020-07-19 17:25:22 +0200

    Update psshlib.py to Python3

--- server/scripts/psshlib.py
+++ server/scripts/psshlib.py
@@ -8,19 +8,21 @@ import subprocess
 
 TESTBED_PI_PATH = "/home/user/scripts"
 
+
 def pssh(hosts_path, cmd, message, inline=False, merge_path=False):
-  print "%s (on all: %s)" %(message, cmd)
-  if merge_path:
-    cmdpth = os.path.join(TESTBED_PI_PATH, cmd)
-  else:
-  	cmdpth = cmd
-  return subprocess.call(["parallel-ssh","--timeout", "240", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-i" if inline else "", cmdpth])
-  
+    print("%s (on all: %s)" % (message, cmd))
+    if merge_path:
+        cmdpth = os.path.join(TESTBED_PI_PATH, cmd)
+    else:
+        cmdpth = cmd
+    return subprocess.call(["parallel-ssh", "--timeout", "240", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-i" if inline else "", cmdpth])
+
+
 def pscp(hosts_path, src, dst, message):
-  print "%s (on all: %s -> %s)" %(message, src, dst)
-  return subprocess.call(["parallel-scp", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-r", src, dst])
+    print("%s (on all: %s -> %s)" % (message, src, dst))
+    return subprocess.call(["parallel-scp", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-r", src, dst])
+
 
 def pslurp(hosts_path, src, dst, message):
-  print "%s (on all: %s -> %s)" %(message, src, dst)
-  return subprocess.call(["parallel-slurp", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-r", "-L", dst, src, "."])
-
+    print("%s (on all: %s -> %s)" % (message, src, dst))
+    return subprocess.call(["parallel-slurp", "-h", hosts_path, "-o", "pssh-out", "-e", "pssh-err", "-l", "user", "-r", "-L", dst, src, "."])
